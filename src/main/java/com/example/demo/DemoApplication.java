233b607c24f31e58b9f9ff84a17045eb0ff13ef7
bala-amypo <bala@amypo.in>
2025-12-26 17:36:20 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,23 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * DemoApplication
+ * ----------------
+ * This is the main entry point of the Spring Boot application.
+ *
+ * Responsibilities:
+ * - Bootstraps the Spring context
+ * - Enables component scanning for com.example.demo
+ * - Starts embedded server (Tomcat)
+ *
+ * This class is REQUIRED for:
+ * - @SpringBootTest
+ * - Application startup
+ * - Integration testing
+ */
+
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -11,3 +28,4 @@ public class DemoApplication {
 	}
 
 }
+
